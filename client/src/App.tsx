cc85d71fca3723bb233e758ff9d5109fae1e850f
SolomonLake <lakegh@gmail.com>
2022-03-01 16:29:04 -0600

    Clean up camera component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,61 +4,7 @@ import { useEffect, useReducer, useRef } from "react";
 import { AvatarCamera } from "./features/avatar/AvatarCamera";
 import { ResizeEngine } from "./features/resize/ResizeEngine";
 
-export type State = {
-  avatar: { absoluteRotation: number; mesh: { position: Vector3 } };
-  temp: {
-    activeInputs: {
-      up: boolean;
-      down: boolean;
-      left: boolean;
-      right: boolean;
-    };
-  };
-};
-export type Inputs = State["temp"]["activeInputs"];
-type Input = keyof Inputs;
-
-type Action =
-  | { type: "inputActivated"; input: Input }
-  | { type: "inputDeactivated"; input: Input };
-
-function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case "inputActivated":
-      return {
-        ...state,
-        temp: {
-          ...state.temp,
-          activeInputs: { ...state.temp.activeInputs, [action.input]: true },
-        },
-      };
-    case "inputDeactivated":
-      return {
-        ...state,
-        temp: {
-          ...state.temp,
-          activeInputs: { ...state.temp.activeInputs, [action.input]: false },
-        },
-      };
-  }
-}
-
 export const App = () => {
-  const [state, dispatch] = useReducer(reducer, {
-    avatar: {
-      absoluteRotation: 0,
-      mesh: { position: new Vector3(0, 0, 0) },
-    },
-    temp: {
-      activeInputs: {
-        up: false,
-        down: false,
-        left: false,
-        right: false,
-      },
-    },
-  });
-
   const framesPerSecond = 60;
   const gravity = -9.81;
 
--- client/src/features/avatar/AvatarCamera.tsx
+++ client/src/features/avatar/AvatarCamera.tsx
@@ -4,30 +4,17 @@ import { useEngine, useScene } from "react-babylonjs";
 
 export const AvatarCamera = ({}: {}) => {
   const engine = useEngine();
-  const scene = useScene();
   const cameraRef = useRef<FreeCamera | null>(null);
 
   useEffect(() => {
-    const camera = cameraRef.current;
     const canvas = engine?.getRenderingCanvas();
-    if (camera && scene && canvas) {
-      scene.activeCamera = camera;
-      camera.attachControl(canvas, true);
-
-      camera.keysUp.push(87);
-      camera.keysLeft.push(65);
-      camera.keysDown.push(83);
-      camera.keysRight.push(68);
-
-      camera.minZ = 0.45;
-
+    if (canvas) {
       canvas.onclick = function () {
         canvas.requestPointerLock =
           canvas.requestPointerLock ||
           canvas.mozRequestPointerLock ||
           canvas.webkitRequestPointerLock;
 
-        // Ask the browser to lock the pointer)
         canvas.requestPointerLock();
       };
     }
@@ -37,6 +24,10 @@ export const AvatarCamera = ({}: {}) => {
     <freeCamera
       name="camera1"
       ref={cameraRef}
+      keysUp={[87]}
+      keysLeft={[65]}
+      keysDown={[83]}
+      keysRight={[68]}
       checkCollisions
       position={new Vector3(0, 5, 0)}
       ellipsoid={new Vector3(1, 1, 1)}
