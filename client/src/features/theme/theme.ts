2824f621fbc8a2e7b7697eb4e61c2f093b8d9022
SolomonLake <lakegh@gmail.com>
2022-04-09 14:55:13 -0500

    Tweak color palatte

--- client/src/features/theme/theme.ts
+++ client/src/features/theme/theme.ts
@@ -1,7 +1,24 @@
 export const PALATTE = {
   dark: "#3D405B",
   light: "#F4F1DE",
-  accent: "#d4d501",
-  team1: "#e3a0c3",
-  team2: "#81B29A",
+  accent: "#7ee670",
+  team1: "#D13F6B",
+  team2: "#66A1CB",
 };
+
+/* 
+#D13F6B
+#9A1B38
+#e85fa7
+#f593c7
+#e3a0c3
+#E18863
+#ffca61
+#ffdd99
+#d4d501
+#c6ff29
+#e4ff9c
+#7ee670
+#81B29A
+#66A1CB
+*/
